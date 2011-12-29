b90c9edd048b891e7493db480d7f9f864849b614
Tor Henning Ueland <tor.henning@gmail.com>
2011-12-29 23:20:50 +0100

    Fixed wrong command prefix for memory plugin

--- plugins/memoryPlugin.php
+++ plugins/memoryPlugin.php
@@ -18,7 +18,7 @@ class memoryPlugin implements pluginInterface {
 	}
 
         function onMessage($from, $channel, $msg) {
-		if(stringEndsWith($msg, '!memory')) {
+		if(stringEndsWith($msg, "{$this->config['trigger']}memory")) {
 			$usedMem = round(((memory_get_usage() / 1024) / 1024),2);
 			$freeMem = round(($this->config['memoryLimit'] - $usedMem),2);
 			sendMessage($this->socket, $channel, $from.": Memory status: {$usedMem} MB used, {$freeMem} MB free.");
